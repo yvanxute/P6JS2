781b182354bd16e29392fd80c29996ae19ce11b5
yvanxute <yusteyvan@gmail.com>
2019-11-06 21:30:54 +0100

    aster

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -9,23 +9,25 @@ class Card {
     }
 
     generatHtml() {
-        
+        // je doit rendre cet fonction universel et reutilisable 
         function htmlPlayer(player) {
             return `<div id='${player.name}'>
             <ul> ${player.name}
-           <p></P>
+            <p></P>
             <span id='health'> santé ${player.health}</span>
-           <p></P>
+            <p></P>
             <span id='pw'> puissance arme ${player.weapon}</span>
             </ul>
             </div> `;
         }
+        console.log('info player', htmlPlayer);
     }
 
 
     createElement(player) {
-        htmlPlayer.id = `cardPlayer-${player.id}`;
-        console.log('info player', htmlPlayer.id);
+        //je doit pouvoir recuperer le generatHtml et lui insert les information de chaque player 
+        htmlPlayer = `cardPlayer-${player}`;
+       
 
         document.querySelector('.js-cardPlayer').innerHTML += htmlPlayer(player);
     }
